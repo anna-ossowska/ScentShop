5cce5543bc4c57216b721b1604d792d1a7f4eeba
Anna Ossowska <ossowska.anna1@gmail.com>
2021-04-03 22:32:57 +0200

    Creating a Controller

---
+++ ScentShop/ScentShop/Controllers/PerfumeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ScentShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ScentShop.Controllers
+{
+    public class PerfumeController : Controller
+    {
+        private readonly IPerfumeRepository _perfumeRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public PerfumeController(IPerfumeRepository perfumeRepository, ICategoryRepository categoryRepository)
+        {
+            this._perfumeRepository = perfumeRepository;
+            this._categoryRepository = categoryRepository;
+        }
+
+        public ViewResult List()
+        {
+            return View(_perfumeRepository.GetAllPerfumes());
+        }
+    }
+}
