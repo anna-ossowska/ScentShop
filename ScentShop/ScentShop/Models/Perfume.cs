21ffef9c652800d6a30f0da855e70b34ab0434f3
Anna Ossowska <ossowska.anna1@gmail.com>
2021-04-03 15:28:26 +0200

    Creating the domain classes

---
+++ ScentShop/ScentShop/Models/Perfume.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ScentShop.Models
+{
+    public class Perfume
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Brand { get; set; }
+        public int Capacity { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+        public string ImageThumbnailUrl { get; set; }
+        public bool IsPerfumeOfTheWeek { get; set; }
+        public bool InStock { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+
+    }
+}
