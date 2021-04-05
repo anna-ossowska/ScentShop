cc16d0c5ffacc4c95d2b18ecf4276e241a9a2a0c
Anna Ossowska <ossowska.anna1@gmail.com>
2021-04-05 15:23:36 +0200

    Adding NuGet Packages and configuring the Entity Framework

--- ScentShop/ScentShop/Startup.cs
+++ ScentShop/ScentShop/Startup.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using ScentShop.Models;
@@ -13,10 +15,19 @@ namespace ScentShop
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            this.Configuration = configuration;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            // AddDbContextPool enables pooling of DbContext instances
+            services.AddDbContext<AppDbContext>(options => 
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             // register framework services
             // support for MVC
             services.AddControllersWithViews();
