9fe1bb49fd2beb2d7915dc38751c3c28b7783264
Anna Ossowska <ossowska.anna1@gmail.com>
2021-04-05 20:25:03 +0200

    Modifying the domain class and updating the DB

--- ScentShop/ScentShop/Migrations/20210405182025_IsDiscountedOnPerfume.Designer.cs
+++ ScentShop/ScentShop/Migrations/20210405182025_IsDiscountedOnPerfume.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ScentShop.Models;
 
 namespace ScentShop.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210405182025_IsDiscountedOnPerfume")]
+    partial class IsDiscountedOnPerfume
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ ScentShop/ScentShop/Migrations/20210405182025_IsDiscountedOnPerfume.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ScentShop.Migrations
+{
+    public partial class IsDiscountedOnPerfume : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDiscounted",
+                table: "Perfumes",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsDiscounted",
+                table: "Perfumes");
+        }
+    }
+}
