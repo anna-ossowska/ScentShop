e30b6c1e57adcc4c4a8aba1a346ff8f44942f208
Anna Ossowska <ossowska.anna1@gmail.com>
2021-04-07 14:11:07 +0200

    Creating the Home Page

---
+++ ScentShop/ScentShop/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ScentShop.Models;
+using ScentShop.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ScentShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IPerfumeRepository _perfumeRepository;
+
+        public HomeController(IPerfumeRepository perfumeRepository)
+        {
+            this._perfumeRepository = perfumeRepository;
+        }
+
+        public IActionResult Index()
+        {
+            HomeViewModel homeViewModel = new HomeViewModel
+            {
+                PerfumesOfTheWeek = _perfumeRepository.PerfumesOfTheWeek()
+            };
+
+            return View(homeViewModel);
+        }
+    }
+}
---
+++ ScentShop/ScentShop/ViewModels/HomeViewModel.cs
@@ -0,0 +1,13 @@
+﻿using ScentShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ScentShop.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<Perfume> PerfumesOfTheWeek { get; set; }
+    }
+}
