5693f430425ed07c9318cc109a067f13767930b5
Anna Ossowska <ossowska.anna1@gmail.com>
2021-04-08 15:59:41 +0200

    Implementing the ShoppingCart Controller + View Models

---
+++ ScentShop/ScentShop/Controllers/ShoppingCartController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ScentShop.Models;
+using ScentShop.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ScentShop.Controllers
+{
+    public class ShoppingCartController : Controller
+    {
+        private readonly IPerfumeRepository _perfumeRepository;
+        private readonly ShoppingCart _shoppingCart;
+
+        public ShoppingCartController(IPerfumeRepository perfumeRepository, ShoppingCart shoppingCart)
+        {
+            this._perfumeRepository = perfumeRepository;
+            this._shoppingCart = shoppingCart;
+        }
+
+        public IActionResult Index()
+        {
+            var items = _shoppingCart.GetShoppingCartItems();
+            _shoppingCart.ShoppingCartItems = items;
+
+            var shoppingCarViewModel = new ShoppingCartViewModel
+            {
+                ShoppingCart = _shoppingCart
+            };
+
+            return View(shoppingCarViewModel);
+        }
+    }
+}
---
+++ ScentShop/ScentShop/ViewModels/ShoppingCartViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ScentShop.Models;
+
+namespace ScentShop.ViewModels
+{
+    public class ShoppingCartViewModel
+    {
+        public ShoppingCart ShoppingCart { get; set; }
+       
+    }
+}
