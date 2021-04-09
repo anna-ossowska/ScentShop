0071df22a7364dead8c0ae5dff35f2e2ee6eaa99
Anna Ossowska <ossowska.anna1@gmail.com>
2021-04-09 21:13:29 +0200

    Implementing the OrderController

---
+++ ScentShop/ScentShop/Controllers/OrderController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ScentShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ScentShop.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly IOrderRepository _orderRepository;
+        private readonly ShoppingCart _shoppingCart;
+
+        public OrderController(IOrderRepository orderRepository, ShoppingCart shoppingCart)
+        {
+            this._orderRepository = orderRepository;
+            this._shoppingCart = shoppingCart;
+        }
+
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+    }
+}
